8c2e5d62db575028d387828dd911d8c8d7f88633
Bonito <75036857+bfwontcodewithme@users.noreply.github.com>
2023-04-23 22:25:18 +0300

    tests done

--- Test.cpp
+++ Test.cpp
@@ -5,11 +5,69 @@
 #include "sources/card.hpp"
 using namespace ariel;
 
-TEST_CASE("jksks"){
+TEST_CASE("Checking condition before starting the game"){
     Player p1("Alice");
     Player p2("Bob");
 
+    Game g(p1,p2);
+    Game g1(p1,p1);
+
     CHECK(p1.cardesTaken() == 0);
     CHECK(p2.cardesTaken() == 0);
+    CHECK(p1.stacksize() == 0);
+    CHECK(p2.stacksize() == 0);
+    
+    CHECK_NOTHROW(g.playTurn());
+    CHECK_NOTHROW(g1.playTurn());
+
+};
+
+TEST_CASE("player gets cards"){         //52 cards total
+    Player p1("Alice");
+    Player p2("Bob");
+    Game g(p1,p2);
+    int total_cards;
+
+    CHECK(p1.stacksize() == 26);    //get start cards
+    CHECK(p1.cardesTaken() == 0);   //before playing any turn
+    CHECK(p2.stacksize() == 26);
+    CHECK(p2.cardesTaken() == 0);
+    total_cards = p1.stacksize() + p2.stacksize();
+    CHECK(total_cards == 52);
+};
+
+TEST_CASE("during the game"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game g(p1,p2);
+
+    CHECK_NOTHROW(g.playTurn());
+
+    CHECK_NOTHROW(g.printLastTurn());
+    CHECK_NOTHROW(g.playAll());
+    CHECK_NOTHROW(g.printWiner());
+};
+TEST_CASE("checking printing logs and stats"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game g(p1,p2);
+    g.playAll();
+    
+    CHECK_NOTHROW(g.printLog());
+    CHECK_NOTHROW(g.printStats());
+    CHECK_NOTHROW(g.printWiner());
+};
+
+TEST_CASE("game finished"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game g(p1,p2);
+
+    g.playAll();
+    int total;
+    total = p1.cardesTaken() + p1.stacksize() + p2.cardesTaken() + p2.stacksize();
+    CHECK(total == 52);   //checking for total crads
+
+    CHECK_THROWS(g.playTurn()); //not posibble after game finished
 
 };
--- sources/game.hpp
+++ sources/game.hpp
@@ -6,23 +6,24 @@ namespace ariel{
         Player p2;
 
         public:
-        
+
+            void playTurn(){};
+            void printLastTurn(){};
+            void playAll(){};
+            void printWiner(){};
+            void printLog(){};
+            void printStats(){};
+
+
             //constructors
             Game(Player player1, Player player2){
-            p1 = player1;
-            p2 = player2;
+                p1 = player1;
+                p2 = player2;
             }
             Game(){         //default game with deafult names
                 p1 = Player();
                 p2 = Player();
             }
-            void playTurn();
-            void printLastTurn();
-            void playAll();
-            void printWiner();
-            void printLog();
-            void printStats();
-
-
+            
     };
 }
